fd542f515d66e9706eb7b2a3f2cfdc4086fc0d74
Spiderfav <r.favinha@pm.me>
2022-11-19 13:29:09 +0000

    Completed all the tests for RevPolish; Calculator finished

--- src/main/java/calculatortypes/RevPolishCalc.java
+++ src/main/java/calculatortypes/RevPolishCalc.java
@@ -1,5 +1,8 @@
 package calculatortypes;
 
+import entry.Symbol;
+import specificstack.NumStack;
+
 /**
  * RevPolishCalc produces a mathematical result from a given Reverse Polish string expression.
  * @author Rui Favinha
@@ -7,15 +10,157 @@ package calculatortypes;
  */
 
 public class RevPolishCalc implements Calculator {
+    
+  private NumStack numberStack = new NumStack();
   
   /**
    * Function to compute a mathematical answer, from a given string expression.
-   * @param calculate given as the string to evaluate.
+   * @param calculate given as the string to calculate result from.
+   * @return The answer calculated as a float.
+   * @throws InvalidExpression when an expression cannot be calculated.
    */
   
-  public float evaluate(String calculate) {
+  public float evaluate(String calculate) throws InvalidExpression {
+          
+    String toEvaluate;
+    int validity;
+    
+    String[] array = calculate.split(" ");
+    
+    // If user just gives one value, return the value to the user as the answer. 
+    
+    if (array.length == 1) {
+      return Float.valueOf(array[0]);
+    }
+    
+    // Checking if the last item is an operation.
+    // Checking that length is odd as all operations have odd length.
+    
+    if (Symbol.convertString(array[array.length - 1]) == Symbol.INVALID || array.length % 2 == 0) {
+      throw new InvalidExpression();
+    }
+    
+    for (int i = 0; i < array.length; i++) {
+      
+      toEvaluate = array[i];
+      validity = checkType(toEvaluate);
+      
+      switch (validity) {
+        
+        case 0:
+          
+          throw new InvalidExpression();
 
-    return Float.valueOf(calculate);
+      
+        case 1:
+          
+          try {
+            float temp1 = numberStack.pop();
+            
+            float temp2 = numberStack.pop();
+
+            numberStack.push(calculate(temp2, temp1, toEvaluate));
+            
+          } catch (Exception e) {
+            throw new InvalidExpression();
+          }
+   
+          break;
+        
+        case 2:
+               
+          numberStack.push(Float.valueOf(toEvaluate));
+          break;
+        
+          
+        // This default statement will never be executed as checkType function only returns 0,1,2.
+        // However, default will sanity check and break out.
+        default:
+          break;
+          
+      }
+      
+    }
+     
+    // Result is at the bottom of the list so we pop once to get result.
+    
+    try {
+        
+      return (numberStack.pop());
+      
+    } catch (Exception e) {
+      
+      throw new InvalidExpression();
+    }
+      
+    
+    
+  }
+  
+  /**
+   * Function to compute a mathematical answer, from a given string expression.
+   * Returns 1 if it is a valid symbol, 2 if it is a valid float and 0 if it is not valid.
+   * @param evaluate given as the string to evaluate.
+   * @return the type for the variable.
+   */
+  
+  public int checkType(String evaluate) {
+    
+    // Symbol is always evaluated and as such we need to check against the symbols we don't want.
+        
+    Symbol symb = Symbol.convertString(evaluate);
+
+    if (symb != Symbol.INVALID && symb != Symbol.LEFT_BRACKET && symb != Symbol.RIGHT_BRACKET) {
+      return 1;
+    }
+    
+    try {
+      
+      // Checking if it can be converted to a float.
+      
+      float numb = Float.valueOf(evaluate);
+      
+      return 2;
+          
+    } catch (Exception e) {
+            
+      // Cannot be converted to symbol or float.
+      
+      return 0;
+    }
+    
+  }
+  
+  /**
+   * Function to compute an answer by applying the operator given to the two given numbers.
+   * @param number1 as the first number, below number2 in the stack.
+   * @param number2 as the second number, the top of the stack.
+   * @param operator as the operator to perform the operation.
+   * @return the result of the given calculation.
+   * @throws InvalidExpression if no operation is given.
+   */
+  
+  public float calculate(float number1, float number2, String operator) throws InvalidExpression {
+    Symbol symb = Symbol.convertString(operator);
+        
+    switch (symb) {
+      case TIMES:
+        return (number1 * number2);
+      
+      case DIVIDE:
+        return (number1 / number2);
+        
+      case PLUS:
+        return (number1 + number2);
+        
+      case MINUS:
+        return (number1 - number2);
+      
+      default: 
+        throw new InvalidExpression();
+
+    }
+    
   }
 
 }
--- src/test/java/calculatortypes/RevPolishCalcTests.java
+++ src/test/java/calculatortypes/RevPolishCalcTests.java
@@ -4,6 +4,7 @@
 package calculatortypes;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -12,17 +13,250 @@ import org.junit.jupiter.api.Test;
  */
 class RevPolishCalcTests {
 
-  @Test // 1
+  // Testing the creation of a new Calculator
+  @Test // Test 1
   void test_creation() {
     RevPolishCalc revPolish = new RevPolishCalc();
   }
   
-  @Test // 2
-  void test_evaluate() {
+  //Changed to account for throws declaration from no operator.
+  // Testing the function to actually calculate the expression returns the result
+  @Test // Test 2
+  void test_calculate_result_plus_simple() throws InvalidExpression {
     RevPolishCalc revPolish = new RevPolishCalc();
-    String calculate = "6";
     
-    assertEquals (revPolish.evaluate(calculate), 6.0);
+    assertEquals (revPolish.calculate(6,0,"+"), 6.0); 
   }
+  
+  //Changed to account for throws declaration from no operator.  
+  // Testing the function to see if it can add two numbers.
+  @Test // Test 3
+  void test_calculate_result_plus() throws InvalidExpression {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    
+    assertEquals (revPolish.calculate(6,5,"+"), 11.0); 
+  }
+
+  //Changed to account for throws declaration from no operator.
+  // Testing the function to see if it can multiply. 
+  @Test // Test 4
+  void test_calculate_result_times_simple() throws InvalidExpression {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    
+    assertEquals (revPolish.calculate(6,1,"*"), 6.0); 
+  }
+  
+  //Changed to account for throws declaration from no operator.
+  //Testing the function to see if it can multiply two numbers.
+  @Test // Test 5
+  void test_calculate_result_times() throws InvalidExpression {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    
+    assertEquals (revPolish.calculate(6,5,"*"), 30.0); 
+  }
+  
+  //Changed to account for throws declaration from no operator.
+  // Simple subtraction.
+  @Test // Test 6
+  void test_calculate_result_minus_simple() throws InvalidExpression {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    
+    assertEquals (revPolish.calculate(6,0,"-"), 6.0); 
+  }
+  
+  // Changed to account for throws declaration from no operator.
+  // More advanced subtraction.
+  @Test // Test 7
+  void test_calculate_result_minus() throws InvalidExpression {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    
+    assertEquals (revPolish.calculate(6,5,"-"), 1.0); 
+  }
+  
+  // Changed to account for throws declaration from no operator.
+  // Simple division
+  @Test // Test 8
+  void test_calculate_result_divide_simple() throws InvalidExpression {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    
+    assertEquals (revPolish.calculate(10,1,"/"), 10.0); 
+  }
+  
+  // Changed to account for throws declaration from no operator.
+  // Advanced division
+  @Test // Test 9 
+  void test_calculate_result_divide() throws InvalidExpression {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    
+    assertEquals (revPolish.calculate(10,5,"/"), 2.0); 
+  }
+  
+  // Testing that if given no operator, exception is thrown.
+  @Test // Test 10
+  void test_calculate_result_no_operator() throws InvalidExpression {
+    RevPolishCalc revPolish = new RevPolishCalc();
+
+    assertThrows(InvalidExpression.class, 
+        () -> {
+          revPolish.calculate(10,5,"");
+        });
+  }
+  
+  // Checking that the function can account for large/small positive numbers.
+  @Test // Test 11
+  void test_calculate_result_large_numbers() throws InvalidExpression {
+    RevPolishCalc revPolish = new RevPolishCalc();
+
+    assertEquals (revPolish.calculate(340282347000000000000000000000000000000f,2,"*"), Float.POSITIVE_INFINITY); 
+  }
+  
+  //Checking that the function can account for large/small negative numbers.
+  @Test // Test 12
+  void test_calculate_result_large_negative_numbers() throws InvalidExpression {
+    RevPolishCalc revPolish = new RevPolishCalc();
+
+    assertEquals (revPolish.calculate(340282347000000000000000000000000000000f,-2,"*"), Float.NEGATIVE_INFINITY); 
+  }
+   
+  // Testing that we can correctly identify symbols.
+  @Test // Test 13
+  void test_type_symbol() {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    
+    assertEquals (revPolish.checkType("/"), 1); 
+  }
+  
+  //Testing that we can correctly identify floats.
+  @Test // Test 14
+  void test_type_float() {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    
+    assertEquals (revPolish.checkType("1.0"), 2); 
+  }
+  
+  //Testing that we can correctly identify non floats and non symbols.
+  @Test // Test 15
+  void test_wrong_type() {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    
+    assertEquals (revPolish.checkType("g"), 0); 
+  }
+  
+  //Testing that we can correctly identify symbols.
+  @Test // Test 16
+  void test_type_symbol2() {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    
+    assertEquals (revPolish.checkType("*"), 1); 
+  }
+  
+  //Testing that we can correctly identify symbols.
+  @Test // Test 17
+  void test_type_symbol3() {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    
+    assertEquals (revPolish.checkType("+"), 1); 
+  }
+  
+  //Testing that we can correctly identify symbols.
+  @Test // Test 18
+  void test_type_symbol4() {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    
+    assertEquals (revPolish.checkType("-"), 1); 
+  }
+  
+  //Testing that the calculator can return the results sent to the calculator.
+  @Test // Test 19
+  void test_evaluate() throws InvalidExpression {
+   RevPolishCalc revPolish = new RevPolishCalc();
+   String calculate = "6";
+   
+   assertEquals (revPolish.evaluate(calculate), 6.0);
+  }
+  
+  // Testing the calculator can perform the calculations correctly.
+  @Test // Test 20
+  void test_evaluate_two_numbs() throws InvalidExpression {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    String calculate = "6 5 +";
+    
+    assertEquals (revPolish.evaluate(calculate), 11.0); 
+  }
+  
+  // Testing that longer expressions still work on the calculator.
+  @Test // Test 21
+  void test_evaluate_long_expression() throws InvalidExpression {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    String calculate = "5 6 7 + * 2 -";
+    
+    assertEquals (revPolish.evaluate(calculate), 63.0);
+  }
+  
+  // Testing that when typing two expressions, both are calculated correctly.
+  @Test // Test 22
+  void test_evaluate_two_expression() throws InvalidExpression {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    String calculate = "5 6 7 + * 2 -";
+    
+    assertEquals (revPolish.evaluate(calculate), 63.0);
+    
+    String calculate2 = "6 5 +";
+    
+    assertEquals (revPolish.evaluate(calculate2), 11.0); 
+    
+  }
+  
+  // Testing that after an expression has been calculated, its result does not stay on the array.
+  @Test // Test 23
+  void test_evaluate_two_expression_wrong() throws InvalidExpression {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    String calculate = "5 6 7 + * 2 -";
+    
+    assertEquals (revPolish.evaluate(calculate), 63.0);
+    
+    String calculate2 = "6 +";
+        
+    assertThrows(InvalidExpression.class, 
+        () -> {
+          revPolish.evaluate(calculate2);
+       });
+    
+  }
+  
+  // Testing that an unfinished expression throws an exception.
+  @Test // Test 24
+  void test_evaluate_unfinished_expression() throws InvalidExpression {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    String calculate = "4 5 - 5 6 +";
+    
+    assertThrows(InvalidExpression.class, 
+        () -> {
+          revPolish.evaluate(calculate);
+        });
+    
+  }
+  
+  // Testing that when dividing by 0, calculator produces result.
+  @Test // Test 25
+  void test_zero_expression() throws InvalidExpression {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    String calculate = "5 0 /";
+    
+    assertEquals (revPolish.evaluate(calculate), Float.POSITIVE_INFINITY);
+
+  }
+  
+  // Testing that when multiplying by 0, calculator provides result.
+  @Test // Test 26
+  void test_zero_expression2() throws InvalidExpression {
+    RevPolishCalc revPolish = new RevPolishCalc();
+    String calculate = "5 0 *";
+    
+    assertEquals (revPolish.evaluate(calculate), 0.0);
+    
+  }
+  
+
   
 }
