b3b53773f1111a0dce416db2cfc705d0751202c6
Favinha Rui (2021) ZKAC432 <rui.favinha.2021@live.rhul.ac.uk>
2022-11-26 23:39:07 +0000

    Finally got all the tests running correctly and converting expressions correctly

--- src/main/java/calculatortypes/StandardCalc.java
+++ src/main/java/calculatortypes/StandardCalc.java
@@ -1,14 +1,152 @@
 package calculatortypes;
 
+import entry.BadTypeException;
+import entry.Symbol;
+import specificstack.OpStack;
+import stack.EmptyStackExtension;
+
 /**
  * @author ruifavinha
  *
  */
 public class StandardCalc implements Calculator {
-  
-  public float evaluate(String toEvaluate) {
+
+  private RevPolishCalc rpCalc = new RevPolishCalc();
+
+  private OpStack values = new OpStack();
+
+
+
+  public float evaluate(String toEvaluate) throws InvalidExpression {
+    return rpCalc.evaluate(convertPolish(toEvaluate));
+  }
+
+
+
+  public String convertPolish(String infix) throws InvalidExpression {
+
+    String prefix = "";
+
+    String[] array = infix.split(" ");
+
+    for (int i = 0; i < array.length; i++) {
+
+      if (checkType(array[i]) == 0) {
+        throw new InvalidExpression();
+      } 
+
+      if (checkType(array[i]) == 1) {
+
+        if (Symbol.convertString(array[i]) == Symbol.RIGHT_BRACKET) {
+          
+          while (true) {
+
+            try {
+              Symbol justPopped = values.pop();
+              
+              if (justPopped == Symbol.LEFT_BRACKET) {
+                break;
+              }
+
+              prefix = prefix + justPopped + " ";
+
+            } catch (Exception e) {
+              throw new InvalidExpression();
+            } 
+          }          
+        }
+        
+        
+        if (values.isEmpty() == false) {
+          
+          
+          Symbol onTop = null;
+          
+          try {
+            onTop = values.pop();
+            values.push(onTop);
+            
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+          
+          Symbol toAdd = Symbol.convertString(array[i]);
+          
+          while (onTop.ordinal() < toAdd.ordinal()) {
+            
+            if (toAdd == Symbol.LEFT_BRACKET || onTop == Symbol.LEFT_BRACKET) { 
+              break; 
+            }
+
+            try {
+              onTop = values.pop();
+              
+              if (onTop == Symbol.LEFT_BRACKET) {
+                values.push(onTop);
+                break;
+              }
+              
+              prefix = prefix + onTop.toString() + " ";
+              
+              if (values.isEmpty()) {
+                break;
+              }              
+            } catch (Exception e) {
+              e.printStackTrace();
+              break;
+            } 
+          }
+                    
+        }
+        
+        if (Symbol.convertString(array[i]) != Symbol.RIGHT_BRACKET) {
+          values.push(Symbol.convertString(array[i]));
+        }
+      }
+
+      if (checkType(array[i]) == 2) {
+
+        prefix = prefix + array[i] + " ";
+
+      }
+
+    }
+
+
+    while (values.isEmpty() == false) {
+      try {
+
+        prefix = prefix + values.pop() + " ";
+
+      } catch (Exception e) {
+        throw new InvalidExpression();
+      } 
+    }
+
+
+    return prefix;
+
+    // Need to check if no other operations and 
+  }
+
+  /**
+   * Function to check the type of a given string value.
+   * Returns 1 if it is a valid symbol, 2 if it is a valid float and 0 if it is not valid.
+   * @param evaluate given as the string to evaluate.
+   * @return the type for the variable.
+   */
+
+  public int checkType(String evaluate) {
+
     
-    return Float.valueOf(toEvaluate);
+    // Symbol can always be evaluated, and as such we need to check against what we don't want.
+
+    Symbol symb = Symbol.convertString(evaluate);
+
+    if (symb != Symbol.INVALID) {
+      return 1;
+    }
     
+    return rpCalc.checkType(evaluate);
   }
 }
--- src/test/java/calculatortypes/StandardCalcTests.java
+++ src/test/java/calculatortypes/StandardCalcTests.java
@@ -3,6 +3,8 @@
  */
 package calculatortypes;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -17,19 +19,84 @@ class StandardCalcTests {
   }
   
   @Test // Test 2
-  void evaluate_simple() {
+  void evaluate_simple() throws InvalidExpression {
     StandardCalc calculator = new StandardCalc();
     
     assert (calculator.evaluate("0") == 0);
   }
   
   @Test // Test 3
-  void evaluate_simple_return() {
+  void evaluate_simple_return() throws InvalidExpression {
     StandardCalc calculator = new StandardCalc();
     
     assert (calculator.evaluate("1") == 1);
   }
   
+  @Test // Test 4
+  void check_type_regular_type() {
+    StandardCalc calculator = new StandardCalc();
+    
+    assertEquals(calculator.checkType("+"), 1);
+  }
   
-
+  @Test // Test 4
+  void check_type_bracket() {
+    StandardCalc calculator = new StandardCalc();
+    
+    assertEquals(calculator.checkType("("), 1);
+    assertEquals(calculator.checkType(")"), 1);
+  }
+  
+  @Test // Test 4
+  void check_convert_digit() throws InvalidExpression {
+    StandardCalc calculator = new StandardCalc();
+    
+    assertEquals(calculator.convertPolish("5"), "5 ");
+  }
+  
+  @Test // Test 4
+  void check_convert_simple() throws InvalidExpression {
+    StandardCalc calculator = new StandardCalc();
+    
+    assertEquals(calculator.convertPolish("5 + 3"), "5 3 + ");
+  }
+  
+  @Test // Test 4
+  void check_convert_wrong() throws InvalidExpression {
+    StandardCalc calculator = new StandardCalc();
+    
+    assertThrows(InvalidExpression.class, 
+        () -> {
+          calculator.convertPolish("g");
+        });
+  }
+  
+  @Test // Test 5
+  void check_convert_medium() throws InvalidExpression {
+    StandardCalc calculator = new StandardCalc();
+    
+    assertEquals(calculator.convertPolish("5 * 3 + 2"), "5 3 * 2 + ");
+  }
+  
+  @Test // Test 5
+  void check_convert_long() throws InvalidExpression {
+    StandardCalc calculator = new StandardCalc();
+    
+    assertEquals(calculator.convertPolish("2 * ( 4 + 2 * 5 ) + 10"), "2 4 2 5 * + * 10 + ");
+  }
+  
+  @Test // Test 5
+  void check_convert_brackets() throws InvalidExpression {
+    StandardCalc calculator = new StandardCalc();
+    
+    assertEquals(calculator.convertPolish("( 5 * 3 + ( 2 * 4 ) - 3 ) + 6"), "5 3 * 2 4 * + 3 - 6 + ");
+  }
+  
+  @Test // Test 5
+  void check_evaluate() throws InvalidExpression {
+    StandardCalc calculator = new StandardCalc();
+    
+    assertEquals(calculator.evaluate("( 5 * 3 + ( 2 * 4 ) - 3 ) + 6"), 26);
+  }
+  
 }
