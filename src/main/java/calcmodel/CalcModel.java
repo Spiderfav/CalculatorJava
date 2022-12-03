8097e0b9457035827a3333b6329529bcec455dbd
Favinha Rui (2021) ZKAC432 <rui.favinha.2021@live.rhul.ac.uk>
2022-12-03 14:12:42 +0000

    Created the Calc model and all its tests. Calculator can also return previous calculated value.

--- src/main/java/calcmodel/CalcModel.java
+++ src/main/java/calcmodel/CalcModel.java
@@ -1,9 +1,46 @@
 package calcmodel;
 
+import calculatortypes.InvalidExpression;
+import calculatortypes.RevPolishCalc;
+import calculatortypes.StandardCalc;
+
 /**
+ * CalcModel is the main calculator program that runs.
+ * It requires a String and a boolean to work.
  * @author Rui Favinha
  *
  */
+
 public class CalcModel {
 
+  private RevPolishCalc prefix = new RevPolishCalc();
+  private StandardCalc infix = new StandardCalc();
+  
+  private float previousResult = 0;
+  
+  /**
+   * Function to return the last result calculated by the calculator.
+   * @return The last answer calculated as a float.
+   */
+  
+  public float getPreviousResult() {
+    return this.previousResult;
+  }
+  
+  /**
+   * Function to compute a mathematical answer, given the type of calculator.
+   * @param expression given as the string to calculate result from.
+   * @param infixOrNot as a boolean value to check if it is to calculate in infix or prefix.
+   * @return The answer calculated as a float.
+   * @throws InvalidExpression when an expression cannot be calculated.
+   */
+  
+  public float evaluate(String expression, boolean infixOrNot) throws InvalidExpression {
+
+    if (infixOrNot) {
+      return (this.previousResult = infix.evaluate(expression));
+    }
+    return (this.previousResult = prefix.evaluate(expression));
+  }
+  
 }
